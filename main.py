a2b4e09c87029fbfbf0e2522bff8836dce4dabed
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-04 16:47:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,76 @@ from philh_myftp_biz.pc import Path, script_dir
 from philh_myftp_biz.gui import GUI
 from philh_myftp_biz.file import YAML
 
-#=============================================
+from functools import partial
 
-config = script_dir(__file__).child('config.yaml')
+def install(url:str):
+    
+    gui.clear()
 
-config = YAML(config).read()
-
-print(f'{config=}')
+    gui.Button(
+        text = 'Back'
+    )
 
 #=============================================
 # GUI
 
 gui = GUI()
 
-gui.title = 'Windows Installer'
+gui.title = 'Cracked Adobe Installer'
 
-gui.Header(config['title'])
+class Page:
 
-gui.Button()
+    home = gui.Page()
+    photoshop = gui.Page()
+    premiere_pro = gui.Page()
+
+#=============================================
+# PAGE: HOME
+
+Page.home.Header('Adobe Installer')
+
+Page.home.Button(
+    text = 'Photoshop',
+    page = Page.photoshop
+)
+
+Page.home.Button(
+    text = 'Premiere Pro',
+    page = Page.premiere_pro
+)
+
+#=============================================
+# PAGE: PHOTOSHOP
+
+Page.photoshop.Header('Photoshop')
+
+Page.photoshop.Button(
+    text = 'back',
+    page = Page.home
+)
+
+Page.photoshop.Button(
+    text = 'Install',
+    func = partial(install, 'https://philh.myftp.biz/Media/Programs/Adobe/Photoshop.zip')
+)
+
+#=============================================
+# PAGE: PREMIERE PRO
+
+Page.premiere_pro.Header('Premiere Pro')
+
+Page.premiere_pro.Button(
+    text = 'back',
+    page = Page.home
+)
+
+Page.premiere_pro.Button(
+    text = 'Install',
+    func = partial(install, 'https://philh.myftp.biz/Media/Programs/Adobe/Premiere Pro.zip')
+)
+
+#=============================================
+
+gui.page = Page.home
 
 gui.run()
